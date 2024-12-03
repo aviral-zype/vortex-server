b70ad3a271b1316898a14ece5971bd29e7fb175c
aviral-zype <aviral.gupta@zype.com>
2024-12-03 17:55:51 +0530

    added krs form

--- api/index.js
+++ api/index.js
@@ -114,6 +114,88 @@ app.post("/submit", async (request, response) => {
   }
 });
 
+app.post("/krsform", async (request, response) => {
+  function getIpFromRequest (request) {
+    try {
+      var ip = request.headers['x-forwarded-for'] ||
+      request.connection.remoteAddress ||
+      request.socket.remoteAddress ||
+      request.connection.socket.remoteAddress;
+      ip = ip.split(',')[0];
+      ip = ip.split(':').slice(-1); //in case the ip returned in a format: "::ffff:146.xxx.xxx.xxx"
+      return ip;
+    } catch (error) {
+      return "";
+    }
+  }
+
+  const { name, phonenumber, email, description, country } = request.body;
+  const ip = getIpFromRequest(request);
+
+  try {
+    // Execute both tasks in parallel
+    const [emailResult] = await Promise.all([
+      resend.emails.send({
+        from: `KRS Group - ${name} <krsgroup@vortexio.tech>`,
+        to: ["aviralgupta6@gmail.com", 'krsgroups96@gmail.com'],
+        subject: `New Submission from ${name}`,
+        html: `<!DOCTYPE html>
+          <html lang="en">
+          <head>
+              <meta charset="UTF-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <style>
+                  body {
+                      font-family: Arial, sans-serif;
+                      line-height: 1.6;
+                      margin: 20px;
+                      color: #333;
+                  }
+                  p {
+                      margin-bottom: 15px;
+                  }
+                  strong {
+                      color: #2c3e50;
+                  }
+                  .header {
+                      font-size: 1.5em;
+                      font-weight: bold;
+                      margin-bottom: 20px;
+                  }
+                  .data {
+                      background-color: #f9f9f9;
+                      padding: 10px;
+                      border-radius: 5px;
+                      border: 1px solid #e0e0e0;
+                  }
+              </style>
+          </head>
+          <body>
+              <p class="header">Hi Vishal,</p>
+              <p>The following Query has been received from <strong>${name}</strong>:</p>
+              <div class="data">
+                  <p><strong>Email:</strong> ${email}</p>
+                  <p><strong>Message:</strong> ${description}</p>
+                  <p><strong>Phone Number:</strong> ${phonenumber}</p>
+                  <p><strong>IP:</strong> ${ip}</p>
+              </div>
+          </body>
+          </html>
+          `,
+      })
+    ]);
+
+    // Close the pool connection
+
+    // Handle response for successful operations
+    response.status(200).json({
+      emailData: emailResult.data,
+    });
+  } catch (error) {
+    // Handle error from any of the operations
+    response.status(400).json({ error });
+  }
+});
 
 let nexPersonId = 3;
 const people = [
